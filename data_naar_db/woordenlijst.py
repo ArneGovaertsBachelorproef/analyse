728fce11bb250acf1d16c4b5a245a1d9c3b654cb
Arne Govaerts <arne.govaerts@student.hogent.be>
2022-05-08 21:39:02 +0200

    woordenlijsten

--- data_naar_db/woordenlijst.py
+++ data_naar_db/woordenlijst.py
@@ -1,4 +1,4 @@
-import sqlite3
+import io, sqlite3
 
 con = sqlite3.connect('../elderspeak_detect.db')
 cur = con.cursor()
@@ -6,24 +6,27 @@ cur = con.cursor()
 cur.execute('DROP TABLE IF EXISTS woordenlijst')
 cur.execute('CREATE TABLE woordenlijst (woord TEXT NOT NULL PRIMARY KEY)')
 
-def verwerk_lijst(lijst: str):
-    with open(lijst + '.txt', 'r') as lijst_bestand:
+def verwerk_lijst(lijst: str) -> bool:
+    with io.open(lijst + '.txt', mode='r', encoding='utf-8') as lijst_bestand:
         while True:
-            woord = lijst_bestand.readline().lstrip().rstrip()
+            woord = lijst_bestand.readline()
             if not woord:
                 break
-
-            woord = str(woord)
+            
+            woord = str(woord.lstrip().rstrip())
             if woord != '' and woord[0] != '#':
                 cur.execute('INSERT OR IGNORE INTO woordenlijst VALUES (?)', [ woord ])
-
-        con.commit()        
+                
+        con.commit()
+    return True   
 
 lijsten = [
     'voornamen_m',
     'voornamen_v',
     'plaatsnamen',
-    'woordenlijst_opentaal',
+    'woordenlijst_opentaal'
 ]
+
+print(list(map(verwerk_lijst, lijsten)))
 
 con.close()
